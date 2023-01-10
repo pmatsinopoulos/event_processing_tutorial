bc087925ea0e11970799052160db66c46a515023
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-01-10 13:44:19 +0200

    Amend app.js to write the broadcast id into the broadcasts table in db

--- live_listening_event_consumer/app.js
+++ live_listening_event_consumer/app.js
@@ -1,4 +1,33 @@
 import { Kafka } from "kafkajs";
+import pg from "pg";
+
+const { Pool } = pg;
+
+const dbUser = process.env.DB_USERNAME;
+const dbHost = process.env.DB_HOST;
+const dbDatabase = process.env.DB_DATABASE;
+const dbPassword = process.env.DB_PASSWORD;
+const dbPort = process.env.DB_PORT;
+
+const dbCredentials = {
+  user: dbUser,
+  host: dbHost,
+  database: dbDatabase,
+  password: dbPassword,
+  port: dbPort,
+};
+
+const pool = new Pool(dbCredentials);
+
+const insertRecordIntoDb = async ({ broadcastId }) => {
+  const sqlStatement = `
+    INSERT INTO broadcasts (broadcast_id) values ($1) RETURNING id
+  `;
+
+  const values = [broadcastId];
+
+  return pool.query(sqlStatement, values);
+};
 
 const kafkaBrokers = process.env.KAFKA_BROKERS.split(",");
 const topicName = process.env.TOPIC_NAME;
@@ -17,10 +46,15 @@ consumer.subscribe({ topic: topicName, fromBeginning: true });
 
 consumer.run({
   eachMessage: async ({ topic, partition, message }) => {
-    console.log({
+    const value = message.value.toString();
+    console.debug({
       partition,
       offset: message.offset,
-      value: message.value.toString(),
+      value,
     });
+
+    const id = await insertRecordIntoDb({ broadcastId: value });
+
+    console.debug(`broadcast record created with id: ${id.rows[0].id}`);
   },
 });
