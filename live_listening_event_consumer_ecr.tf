b97a901c48f616975f14551f6bcc2b7595c8fdf8
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-01-27 14:52:57 +0200

    Starting the docker provider, but will not finish it

--- live_listening_event_consumer_ecr.tf
+++ live_listening_event_consumer_ecr.tf
@@ -19,32 +19,54 @@ resource "aws_ecr_repository" "live_listening_event_consumer" {
     "project"     = var.project
   }
 
-  provisioner "local-exec" {
-    working_dir = "./live_listening_event_consumer"
-    command     = "docker build -t ${self.name} ."
-  }
+  # provisioner "local-exec" {
+  #   working_dir = "./live_listening_event_consumer"
+  #   command     = "docker build -t ${self.name} ."
+  # }
 
-  provisioner "local-exec" {
-    working_dir = "./live_listening_event_consumer"
-    command     = "docker tag ${self.name}:latest ${self.repository_url}:latest"
-  }
+  # provisioner "local-exec" {
+  #   working_dir = "./live_listening_event_consumer"
+  #   command     = "docker tag ${self.name}:latest ${self.repository_url}:latest"
+  # }
 
-  provisioner "local-exec" {
-    working_dir = "./live_listening_event_consumer"
-    command     = "docker push ${self.repository_url}:latest"
-  }
+  # provisioner "local-exec" {
+  #   working_dir = "./live_listening_event_consumer"
+  #   command     = "docker push ${self.repository_url}:latest"
+  # }
 
   # Destroy Phase
   # -------------
-  provisioner "local-exec" {
-    when        = destroy
-    working_dir = "./live_listening_event_consumer"
-    command     = "docker image rm --force ${self.name}:latest || true"
+  # provisioner "local-exec" {
+  #   when        = destroy
+  #   working_dir = "./live_listening_event_consumer"
+  #   command     = "docker image rm --force ${self.name}:latest || true"
+  # }
+
+  # provisioner "local-exec" {
+  #   when        = destroy
+  #   working_dir = "./live_listening_event_consumer"
+  #   command     = "docker image rm --force ${self.repository_url}:latest || true"
+  # }
+}
+
+resource "docker_image" "live_listening_event_consumer" {
+  name = "${var.project}/live_listening_event_consumer"
+  build {
+    context = "./live_listening_event_consumer"
+    tag     = ["${var.project}/live_listening_event_consumer:latest", "${aws_ecr_repository.live_listening_event_consumer.repository_url}:latest"]
+    auth_config {
+      host_name = aws_ecr_repository.live_listening_event_consumer.repository_url
+    }
+  }
+  triggers = {
+    dir_sha1 = sha1(join("", [for f in fileset(path.module, "live_listening_event_consumer/*") : filesha1(f)]))
   }
 
-  provisioner "local-exec" {
-    when        = destroy
-    working_dir = "./live_listening_event_consumer"
-    command     = "docker image rm --force ${self.repository_url}:latest || true"
-  }
+  force_remove = true
+  keep_locally = false
+}
+
+resource "docker_registry_image" "live_listening_event_consumer" {
+  name          = docker_image.live_listening_event_consumer.name
+  keep_remotely = false
 }
--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.1"
+    }
   }
   required_version = ">= 1.2.0"
 }
